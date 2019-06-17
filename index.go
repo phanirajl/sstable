a57d0dcb7abe847b01f3c307207095e9df8aa034
Thanh Pham Kieu <thanhpk@live.com>
2019-06-17 23:33:40 +0700

    add decodeVarint

--- index.go
+++ index.go
@@ -46,7 +46,7 @@ func read_index(filepath string) ([]IndexEntry, error) {
 
 	index := make([]IndexEntry, 0)
 	// fill header
-	i := 10
+	i := 100
 	for {
 		i--
 		if i == 0 {
@@ -56,19 +56,14 @@ func read_index(filepath string) ([]IndexEntry, error) {
 		entry.KeyLength = binary.BigEndian.Uint16(data[0:2])
 		entry.Key = data[2 : 2+entry.KeyLength]
 		data = data[2+entry.KeyLength:]
-		position, n := binary.Uvarint(data)
+		position, n := decodeVarint(data)
 		entry.Position = uint64(position)
-		for u := 0; u < n; u++ {
-			println("SSS", data[u])
-		}
 		data = data[n:]
-
-		prom_index_len, n2 := binary.Uvarint(data)
-
+		prom_index_len, n2 := decodeVarint(data)
 		data = data[n2:]
 
 		entry.PromotedIndexLength = uint32(prom_index_len)
-		println("KEY", entry.KeyLength, position, "PROM INDEX LEN", prom_index_len, "N", n, "N2", n2)
+		println("KEY", string(entry.Key), position, "PROM INDEX LEN", prom_index_len, "N", n, "N2", n2)
 
 		entry.PromotedIndex = data[:entry.PromotedIndexLength]
 
@@ -80,4 +75,68 @@ func read_index(filepath string) ([]IndexEntry, error) {
 		}
 	}
 	return nil, nil
+}
+
+// decodeVarint decodes an uint64 from buf and returns that value and the
+// number of bytes read (> 0)
+// the internal representation of varints is explained in:
+// https://haaawk.github.io/2018/02/26/sstables-variant-integers.html
+// Note: the first byte of buf will be modified for performance reason
+func decodeVarint(buf []byte) (x uint64, n int) {
+	first := buf[0]
+
+	// reading 0 byte
+	if first&128 == 0 {
+		return uint64(first), 1
+	}
+
+	// 9 bytes
+	if first == 0xff {
+		return binary.LittleEndian.Uint64(buf[1:9]), 9
+	}
+
+	// 2 bytes
+	if first&128 == 128 {
+		buf[0] &= 191
+		return uint64(binary.LittleEndian.Uint16(buf[0:2])), 2
+	}
+
+	// 3 bytes
+	if first&96 == 96 {
+		buf[0] &= 223
+		return uint64(binary.LittleEndian.Uint32(buf[0:3])), 3
+	}
+
+	// 4 bytes
+	if first&224 == 224 {
+		buf[0] &= 239
+		return uint64(binary.LittleEndian.Uint32(buf[0:4])), 4
+	}
+
+	// 5 bytes
+	if first&240 == 240 {
+		buf[0] &= 247
+		return binary.LittleEndian.Uint64(buf[0:5]), 5
+	}
+
+	// 6 bytes
+	if first&248 == 248 {
+		buf[0] &= 251
+		return binary.LittleEndian.Uint64(buf[0:6]), 6
+	}
+
+	// 7 bytes
+	if first&252 == 252 {
+		buf[0] &= 253
+		return binary.LittleEndian.Uint64(buf[0:7]), 7
+	}
+
+	// 8 bytes
+	if first&254 == 254 {
+		buf[0] &= 254
+		return binary.LittleEndian.Uint64(buf[0:8]), 8
+	}
+
+	// The number is too large to represent in a 64-bit value.
+	return 0, 0
 }
