23e96e56f2f8b80cb9b1811f669d1e7c9110ce0c
Thanh Pham Kieu <thanhpk@live.com>
2019-06-18 01:14:00 +0700

    add data structs

--- data.go
+++ data.go
@@ -1,14 +1,79 @@
 package sstable
 
 type DeletionTime struct {
-	local_deletion_time  uint32
-	marked_for_delete_at uint64
+	LocalDeletionTime uint32
+	MarkedForDeleteAt uint64
 }
 
 type ClusteringBlock struct {
 	ClusteringBlockHeader int
-	clustering_cells      []SimpleCell
+	ClusteringCells       []SimpleCell
 }
 
 type SimpleCell struct {
+	Cell
+	Flags                   byte
+	DeltaTimestamp          uint64
+	DeltaLocalDeletion_time uint64
+	DeltaTtl                uint64
+	Path                    CellPath // only in cells nested into complex_cells
+	Value                   CellValue
+}
+
+type Unfiltered struct{}
+
+type CellPath struct {
+	Length uint64
+	Value  []byte
+}
+
+type CellValue struct {
+	Length uint64
+	Value  []byte
+}
+
+type Partition struct {
+	Header      PartitionHeader
+	StaticRow   Row // Has IS_STATIC flag set
+	Unfiltereds []Unfiltered
+}
+
+type Row struct {
+	Flags         byte
+	ExtendedFlags byte // optional
+
+	// only present for non-static rows
+	ClusteringBlocks   []ClusteringBlock
+	RowBodySize        uint64
+	PrevUnfilteredSize uint64 // for backward traversing
+	LivenessInfo       LivenessInfo
+	DeletionTime       DeltaDeletionTime
+	MissingColumns     []uint64
+	Cells              []Cell
+}
+
+type Cell struct{}
+
+type ComplexCell struct {
+	Cell
+	ComplexDeletionTime DeltaDeletionTime
+	ItemsCount          uint64
+	Items               []SimpleCell
+}
+
+type DeltaDeletionTime struct {
+	DeltaMarkedForDeleteAt uint64
+	DeltaLocalDeletionTime uint64
+}
+
+type LivenessInfo struct {
+	DeltaTimestamp         uint64
+	DeltaTtl               uint64
+	DeltaLocalDeletionTime uint64
+}
+
+type PartitionHeader struct {
+	KeyLength    uint16
+	Key          []byte
+	DeletionTime DeletionTime
 }
